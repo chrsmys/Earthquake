ab66aa60f00577313237c5a65595805bba1297ee
Chris Mays <chrsmys@gmail.com>
2015-02-20 17:16:01 -0500

    Create Event Object

---
+++ Earthquake/CamEvent.h
@@ -0,0 +1,39 @@
+//
+//  CamEvent.h
+//  Earthquake
+//
+//  Created by Chris Mays on 2/20/15.
+//  Copyright (c) 2015 Chris Mays. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CamEvent : NSObject
+
+-(id)initWithFeatureObject:(NSDictionary *)feature;
+
+@property (nonatomic, retain) NSString *magnitudeType; //Measurement that magnitude is in
+@property (nonatomic) double magnitude;
+
+@property (nonatomic) double intensity;
+
+@property (nonatomic) double latitude;
+@property (nonatomic) double longitude;
+
+@property (nonatomic) long totalStationsReported;
+
+@property (nonatomic, retain) NSString *place;
+
+@property (nonatomic) int signifigance;
+
+@property (nonatomic, retain) NSString *status;
+
+@property (nonatomic) long long time;
+
+@property (nonatomic, retain) NSString *type;
+
+@property (nonatomic, retain) NSURL *url;
+
+@property (nonatomic) int timezone;
+
+@end
---
+++ Earthquake/CamEvent.m
@@ -0,0 +1,17 @@
+//
+//  CamEvent.m
+//  Earthquake
+//
+//  Created by Chris Mays on 2/20/15.
+//  Copyright (c) 2015 Chris Mays. All rights reserved.
+//
+
+#import "CamEvent.h"
+
+@implementation CamEvent
+-(id)initWithFeatureObject:(NSDictionary *)feature{
+    self = [super init];
+    
+    return self;
+}
+@end
