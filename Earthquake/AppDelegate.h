71df4cf81cdb99a70a885de472b21889fed47c39
Chris Mays <chrsmys@gmail.com>
2015-02-20 14:46:31 -0500

    Initial Commit

---
+++ Earthquake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Earthquake
+//
+//  Created by Chris Mays on 2/20/15.
+//  Copyright (c) 2015 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EarthquakeTests/EarthquakeTests.m
@@ -0,0 +1,40 @@
+//
+//  EarthquakeTests.m
+//  EarthquakeTests
+//
+//  Created by Chris Mays on 2/20/15.
+//  Copyright (c) 2015 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EarthquakeTests : XCTestCase
+
+@end
+
+@implementation EarthquakeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
