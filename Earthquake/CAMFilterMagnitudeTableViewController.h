952edea936400cc838024bb2d2af358ff7c0bef8
Chris Mays <chrsmys@gmail.com>
2015-02-23 07:54:55 -0500

    Add template for Filtering Magnitude

---
+++ Earthquake/CAMFilterMagnitudeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAMFilterMagnitudeTableViewController.h
+//  Earthquake
+//
+//  Created by Chris Mays on 2/23/15.
+//  Copyright (c) 2015 Chris Mays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAMFilterMagnitudeTableViewController : UITableViewController
+
+@end
